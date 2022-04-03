eb6a7b02ef9f5d939913311554a33ff7666b3228
Muhammad Usman Bin Farid <99407553+UsmanUet276@users.noreply.github.com>
2022-04-03 17:23:13 +0500

    Add files via upload

---
+++ BL/Employ.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS1.BL
+{
+    internal class Employ
+    {
+        public string emp_nameA;
+        public float emp_payA;
+        public float emp_rewardA;
+        public float emp_attendanceA;
+        public float emp_absenceA;
+        public float emp_deductA;
+        public int emp_rankA;
+        public int emp_perA;
+        public int emp_comp_taskA;
+        public bool payA;
+        public string task_1A;
+        public string task_2A;
+        public string task_3A;
+        public string task_4A;
+        public int task1_timeA;
+        public int task2_timeA;
+        public int task3_timeA;
+        public int task4_timeA;
+    }
+}
---
+++ BL/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS1.BL
+{
+    internal class Login
+    {
+        public string usernameA;
+        public string passwordA;
+        public string initialPassA;
+        public string rolesA;
+    }
+}
